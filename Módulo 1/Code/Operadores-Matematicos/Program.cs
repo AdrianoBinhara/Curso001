50c81eab9f8a123bf364b86eebc239c6df6fddb7
Adriano D'lucca Binhara <62717202+AdrianoBinhara@users.noreply.github.com>
2022-01-10 21:03:35 -0300

    Operadores matemáticos

---
+++ Módulo 1/Code/Operadores-Matematicos/Program.cs
@@ -0,0 +1,20 @@
+﻿// Adição 
+
+// Subtração
+
+// Multiplicação
+
+// Divisão
+
+// Exponenciação 
+
+// Ordem de precedência
+
+// x = 5 + 2 * 3
+
+// 1º Parênteses
+// 2º Expoentes
+// 3º Multiplicação e divisão (da esquerda pra direita)
+// 4º Somas e Subtrações (da esquerda pra direita)
+
+// x = 6 + 9 - 2ˆ2 + 9 / 3
