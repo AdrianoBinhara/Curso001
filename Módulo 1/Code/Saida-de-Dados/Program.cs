c3b8b4b2e9e36db3485e20b71598cde15f29e829
Adriano D'lucca Binhara <62717202+AdrianoBinhara@users.noreply.github.com>
2022-01-20 22:10:54 -0300

    alterando codigo

--- Módulo 1/Code/Saida-de-Dados/Program.cs
+++ Módulo 1/Code/Saida-de-Dados/Program.cs
@@ -7,26 +7,26 @@ namespace DataOut
         public static void Main(string[] args)
         {
             //Saída de dados
-            // string name = "Binhara";
-            // string surname = "Adriano";
-            // char gender = 'M';
-            // int age = 22;
-            // double balance = 256.97556;
+            string name = "Binhara";
+            string surname = "Adriano";
+            char gender = 'M';
+            int age = 22;
+            double balance = 256.97556;
 
-            // Console.Write("Olá, {0} {1}", name, surname); //Concatenação
-            // Console.WriteLine($" - {age}/ {gender}"); //Interpolação
-            // Console.WriteLine("Você possui " + balance.ToString() + " em sua conta bancária."); //Concatenação
+            Console.Write("Olá, {0} {1}", name, surname); //Concatenação
+            Console.WriteLine($" - {age}/ {gender}"); //Interpolação
+            Console.WriteLine("Você possui " + balance.ToString("F2", CultureInfo.InvariantCulture) + " em sua conta bancária."); //Concatenação
 
             // ------------------------------------------- //
             //Entrada de dados
-            Console.Write("Quanto dinheiro você possui? ");
-            int money = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            Console.WriteLine($"Você possui {money} reais.");
-            Console.WriteLine();
-            Console.Write("Digite um valor para somar ao seu saldo: ");
-            int deposit = Convert.ToInt32(Console.ReadLine());
-            money += deposit;
-            Console.WriteLine($"{deposit} reais somados! Agora você possui {money} reais.");
+            // Console.Write("Quanto dinheiro você possui? ");
+            // int money = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            // Console.WriteLine($"Você possui {money} reais.");
+            // Console.WriteLine();
+            // Console.Write("Digite um valor para somar ao seu saldo: ");
+            // int deposit = Convert.ToInt32(Console.ReadLine());
+            // money += deposit;
+            // Console.WriteLine($"{deposit} reais somados! Agora você possui {money} reais.");
         }
     }
 }
